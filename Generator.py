8a69115fb490fef4c3cc5769ab8a133e2f7cb58f
Ilay <ilaybl125@gmail.com>
2020-08-05 23:20:41 +0200

    Minor bug fixing

--- Generator.py
+++ Generator.py
@@ -6,15 +6,15 @@ class Generator:
         self.start = (random.randrange(0, rows), random.randrange(0, cols))
         self.Grid = g(rows, cols, self.start)
         self.loc = self.start
-        self.way = [self.start]
+        self.way = []
         self.rows = rows
         self.cols = cols
 
         self.Grid.grid[self.start[0]][self.start[1]].possible = False
 
     def deadend(self):
-        r, c = self.loc
-        cell = self.Grid.grid[r][c]
+        c, r = self.loc
+        cell = self.Grid.grid[c][r]
 
         cell.possible = False
         self.way.remove(self.loc)
@@ -22,26 +22,26 @@ class Generator:
         self.loc = self.way[-1]
 
     def turn(self, loc):
-        r, c = self.loc
+        c, r = self.loc
         poss = []
         found = False
 
         while not found:
 
-            if c-1 > -1:
-                if not loc.up and not self.Grid.grid[r][c - 1].been and self.Grid.grid[r][c - 1].possible:
+            if c > 0:
+                if not loc.up and not self.Grid.grid[c-1][r].been and self.Grid.grid[c - 1][r].possible:
                     poss.append("up")
                     found = True
-            if c+1 < self.cols:
-                if not loc.down and not self.Grid.grid[r][c + 1].been and self.Grid.grid[r][c + 1].possible:
+            if c + 1 < self.cols:
+                if not loc.down and not self.Grid.grid[c+1][r].been and self.Grid.grid[c + 1][r].possible:
                     poss.append("down")
                     found = True
-            if r-1 > -1:
-                if not loc.left and not self.Grid.grid[r - 1][c].been and self.Grid.grid[r - 1][c].possible:
+            if r > 0:
+                if not loc.left and not self.Grid.grid[c][r-1].been and self.Grid.grid[c][r - 1].possible:
                     poss.append("left")
                     found = True
-            if r+1 < self.rows:
-                if not loc.right and not self.Grid.grid[r + 1][c].been and self.Grid.grid[r + 1][c].possible:
+            if r + 1 < self.rows:
+                if not loc.right and not self.Grid.grid[c][r+1].been and self.Grid.grid[c][r + 1].possible:
                     poss.append("right")
                     found = True
 
@@ -53,37 +53,38 @@ class Generator:
         return random.choice(poss)
 
     def move(self):
-        print(self.way)
-
-        r, c = self.loc
-        cell = self.Grid.grid[r][c]
+        c, r = self.loc
+        cell = self.Grid.grid[c][r]
 
         cell.been = True
+
+        print(self.loc)
+
         self.way.append(self.loc)
 
         turn = self.turn(cell)
         if turn == "up":
             cell.up = True
-            self.Grid.grid[r][c - 1].down = True
+            self.Grid.grid[c - 1][r].down = True
 
-            self.loc = (r, c - 1)
+            self.loc = (c - 1, r)
 
         elif turn == "down":
             cell.down = True
-            self.Grid.grid[r][c + 1].up = True
+            self.Grid.grid[c + 1][r].up = True
 
-            self.loc = (r, c + 1)
+            self.loc = (c + 1, r)
 
         elif turn == "left":
             cell.left = True
-            self.Grid.grid[r - 1][c].right = True
+            self.Grid.grid[c][r-1].right = True
 
-            self.loc = (r - 1, c)
+            self.loc = (c, r-1)
 
         elif turn == "right":
             cell.right = True
-            self.Grid.grid[r + 1][c].left = True
+            self.Grid.grid[c][r + 1].left = True
 
-            self.loc = (r + 1, c)
+            self.loc = (c, r + 1)
 
 
--- Test.py
+++ Test.py
@@ -15,6 +15,7 @@ cell_width = WIDTH//cols
 cell_height = HEIGHT//rows
 
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -25,7 +26,7 @@ def main():
     gen = Generator(rows, cols)
 
     while True:
-        win.fill((255, 255, 255))
+        win.fill(WHITE)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,6 +40,21 @@ def main():
             pygame.draw.line(win, BLACK, (i*cell_width, 0), (i*cell_width, HEIGHT), 5)
 
         gen.move()
+
+        for r in range(rows):
+            for c in range(cols):
+                if gen.Grid.grid[c][r].up:
+                    pygame.draw.line(win, RED, (c*cell_width, r*cell_height), ((c-1)*cell_width, r*cell_height), 5)
+
+                if gen.Grid.grid[c][r].down:
+                    pygame.draw.line(win, RED, (c*cell_width, r*cell_height), ((c+1)*cell_width, r*cell_height), 5)
+
+                if gen.Grid.grid[c][r].left:
+                    pygame.draw.line(win, RED, (c*cell_width, r*cell_height), (c*cell_width, (r-1)*cell_height), 5)
+
+                if gen.Grid.grid[c][r].right:
+                    pygame.draw.line(win, RED, (c*cell_width, r*cell_height), (c*cell_width, (r+1)*cell_height), 5)
+
 
         clock.tick(FPS)
         pygame.display.update()

4e582fc41cd1b88bc07b5b20ddc88c730d3cacec
Saturn <ronron3306@gmail.com>
2020-08-17 00:09:00 +0300

    added menu function

--- Gaem.py
+++ Gaem.py
@@ -1,5 +1,20 @@
 import pygame
+import threading
 from Classes.Generator import Generator
+
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 1000
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
 
 def generation(rows, cols):
     gen = Generator(rows, cols)
@@ -9,3 +24,15 @@ def generation(rows, cols):
 
     return gen.Grid.grid, gen.start, gen.last
 
+def menu():
+    while True:
+        win.fill(WHITE)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+
+
+    clock.tick(30)
+    pygame.display.update()

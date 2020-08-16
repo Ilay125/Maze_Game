2aed64b62d4446bc5b987ed0e25880350b8b0120
Ilay <ilaybl125@gmail.com>
2020-08-17 00:30:26 +0300

    Added button function

--- Gaem.py
+++ Gaem.py
@@ -4,17 +4,39 @@ from Classes.Generator import Generator
 
 pygame.init()
 
-WIDTH = 1000
+WIDTH = 1200
 HEIGHT = 1000
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+DARKRED = (180, 0, 0)
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
+
+def action(args):
+    pass
+
+def button(msg, x, y, w, h, ic, ac, xoffset = 10, yoffset = 10, font="arial", fontSize=30, tcolor=BLACK, action=None, args=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(win, ac, (x, y, w, h))
+        if click[0] == 1 and action is not None:
+            if args is not None:
+                action(args)
+            else:
+                action()
+    else:
+        pygame.draw.rect(win, ic, (x, y, w, h))
+
+    font = pygame.font.SysFont(font, fontSize, True)
+    screen_text = font.render(msg, True, tcolor)
+    win.blit(screen_text, (x+(w/2)+xoffset, y+(h/2)+yoffset))
+
 
 def generation(rows, cols):
     gen = Generator(rows, cols)
@@ -32,7 +54,10 @@ def menu():
                 pygame.quit()
                 quit()
 
+        button("Start", WIDTH//2-75, HEIGHT//2-25, 150, 50, 10, -15, DARKRED, RED)
+        button("Custom", WIDTH // 2 - 75, HEIGHT // 2 + 50, 150, 50, 10, -15, DARKRED, RED)
 
+        clock.tick(30)
+        pygame.display.update()
 
-    clock.tick(30)
-    pygame.display.update()
+menu()
